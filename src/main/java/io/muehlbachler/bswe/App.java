d0a1513e95e8492cac90fb184d1abc494b40224a
Leon <160027005+LeonFHBurgenland@users.noreply.github.com>
2024-10-19 16:19:12 +0200

    testing geradle rule again ...

--- src/main/java/io/muehlbachler/bswe/App.java
+++ src/main/java/io/muehlbachler/bswe/App.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
-    //Change
+    //Change it
     public static void main(final String[] args) {
         SpringApplication.run(App.class, args);
     }
